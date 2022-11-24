6ec28930e4e1b1b732d9df179d25f7098c436d80
Philipp Weiß <p.weiss85@gmx.de>
2022-11-24 16:13:36 +0100

    add first dummy layout

---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,17 @@
+import "./Task.css"
+
+function Task () {
+    return ( 
+        <section className="taskcontainer">
+            <article className="taskcontainer-content ">
+                <button>activate</button>
+                <p>icon</p>
+                <p>test</p>
+                <p>timer</p>
+                <p>status</p>
+            </article>
+        </section>
+    );
+}
+
+export default Task;
