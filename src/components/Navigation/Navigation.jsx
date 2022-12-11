3919e671072dc2c5820d8183943aa80bf3d23097
Philipp Weiß <p.weiss85@gmx.de>
2022-12-11 19:34:02 +0100

    update footer menu

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,7 +6,7 @@ function Navigation() {
         <nav className="navmenu">
             <ul>
             <li className="navitem"><a href="#">HOME</a></li>  
-            <div className="navitem--container"></div>
+            <div className="navitem--container"><p className="navitem--text">+</p></div>
             <div className="navitem--separator"></div>
             <li className="navitem"><a href="#">STATS</a></li>
             </ul>
