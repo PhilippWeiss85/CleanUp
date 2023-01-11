e1e60ed4ed7f9620d3c35483b4923263663a2b52
Philipp <philipp@MacBook-Air.fritz.box>
2023-01-11 10:50:24 +0100

    update

--- src/components/Modal/TaskModal.jsx
+++ src/components/Modal/TaskModal.jsx
@@ -22,7 +22,7 @@ function TaskModal({toggleActive, closeModal, task, responsible}) {
             </article>
             <label>Wie lange gebraucht?</label>
             <article className="taskmodal--section">
-            <input className="modalinput" type="number"/>
+            <input className="modalinput" type="number" step="10" min="0" max="1000"/>
             <p>Minuten</p>
             </article>
             <button onClick={toggleActive} className="savebutton">save</button>
