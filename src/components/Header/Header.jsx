a80bd798a60654be60e34d25562d26616a425f48
Philipp <p.weiss85@gmx.de>
2023-01-25 17:13:03 +0100

    update css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,9 @@ import "./Header.css"
 function Header() {
     return (
         <section className="header">
-            <h1 className="header--title">CleanUp</h1>
-            <p className="header--subtitle">Your personal cleaning scheduler</p>
+            <h1>CleanUp</h1>
+            <img className="logo" alt="animated logo" src="src/assets/lottie_logo.gif"></img>
+            <p >Your personal cleaning scheduler</p>
         </section>
      );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,12 +8,9 @@ function Navigation() {
     return (
         <>
         <nav className="navmenu">
-            <ul>
             <li className="navitem"><Link to="/">HOME</Link></li>  
-            <div className="navitem--container"><Link to="/newtask"><p className="navitem--text">+</p></Link></div>
-            <div className="navitem--separator"></div>
+            <li className="navitem"><div className="navitem--container"><Link to="/newtask"><p className="navitem--text">+</p></Link></div></li>
             <li className="navitem"><Link to="/statistics">STATS</Link></li>
-            </ul>
         </nav>
         </>
      );
